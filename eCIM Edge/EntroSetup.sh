c2d23f39ae2d28a7ce9cc2189affe63d3895f3e6
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2019-03-06 15:41:59 -0500

    debugging

--- eCIM Edge/EntroSetup.sh
+++ eCIM Edge/EntroSetup.sh
@@ -54,13 +54,13 @@ echo ""
 apt-get install -y unzip htop default-jre fail2ban
 
 #Set Fail2Ban Options
-if [ grep -Fxq 'bantime  = -1' /etc/fail2ban/jail.conf ]; then
+if grep -Fxq "bantime  = -1" /etc/fail2ban/jail.conf; then
   echo "Exists"
 else
   echo "Does noy Exist"
-#  if [ -e /etc/fail2ban/jail.conf ]; then
-#    sed -i -e 's/bantime  = 600/bantime  = -1/g' /etc/fail2ban/jail.conf
-#  fi
+  if [ -e /etc/fail2ban/jail.conf ]; then
+    sed -i -e 's/bantime  = 600/bantime  = -1/g' /etc/fail2ban/jail.conf
+  fi
 fi
 
 
