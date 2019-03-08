806d58748f1651f05a580f8cb7ff31493a415e80
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2019-03-07 21:11:47 -0500

    fix bash run

--- podsync/_LiveScripts/ll-backup.sh
+++ podsync/_LiveScripts/ll-backup.sh
@@ -4,6 +4,7 @@ BACKDEST=https://nextcloud.heptasystems.com:8443/nextcloud/s/ZTsdkpGk3ECgwrX
 LATESTSNAP=`ls -rt /apps/finstack/db/londonLife/snapshots/* | tail -1`
 BACKFILE=$HOSTNAME-Snapshot.zip
 LOGLOC=/data/hepta
+cDIR='PWD'
 
 if [ ! -f $LOGLOC/$BACKFILE ]; then
   cp $LATESTSNAP $LOGLOC/$BACKFILE
@@ -17,6 +18,6 @@ fi
 
 diff -q $LOGLOC/latestsnap.log $LOGLOC/priorsnap.log > /dev/null
 if [ $? -ne 0 ] ; then
-  ~/cloudsend.sh $LOGLOC/$BACKFILE $BACKDEST
+  bash $cDIR/cloudsend.sh $LOGLOC/$BACKFILE $BACKDEST
   cat $LOGLOC/latestsnap.log > $LOGLOC/priorsnap.log
 fi
