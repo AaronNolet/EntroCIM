b6322650cd12f541a9371bbb2eda17037b76907b
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2019-03-07 20:32:38 -0500

    ll-backup script creation

---
+++ podsync/_LiveScripts/ll-backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+HOSTNAME=$(hostname)
+BACKDEST=https://nextcloud.heptasystems.com:8443/nextcloud/s/ZTsdkpGk3ECgwrX
+LATESTSNAP=`ls -rt /apps/finstack/db/*/snapshots/* | tail -1`
+BACKFILE=$HOSTNAME-Snapshot.zip
+LOGLOC=/data/hepta
+
+if [ ! -e $LOGLOC/$BACKFILE ]; then
+  cp $LATESTSNAP > $LOGLOC/$BACKFILE
+fi
+
+echo $LATESTSNAP > $LOGLOC/latestsnap.log
+
+if [ ! -e $LOGLOC/priorsnap.log]; then
+  cp $LOGLOC/latestsnap.log > $LOGLOC/priorsnap.log
+fi
+
+diff -q $LOGLOC/latestsnap.log $LOGLOC/priorsnap.log > /dev/null
+if [ $? -ne 0 ] ; then
+  ~/cloudsend.sh $LOGLOC/$BACKFILE $BACKDEST
+  cp $LOGLOC/latestsnap.log > $LOGLOC/priorsnap.log
+fi
