f8a21a533a33450eafb16258779325264fe27e0d
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2019-03-07 21:51:53 -0500

    final?

--- podsync/_LiveScripts/ll-backup.sh
+++ podsync/_LiveScripts/ll-backup.sh
@@ -4,11 +4,6 @@ BACKDEST=https://nextcloud.heptasystems.com:8443/nextcloud/s/ZTsdkpGk3ECgwrX
 LATESTSNAP=`ls -rt /apps/finstack/db/londonLife/snapshots/* | tail -1`
 BACKFILE=$HOSTNAME-Snapshot.zip
 LOGLOC=/data/hepta
-cDIR='PWD'
-
-if [ ! -f $LOGLOC/$BACKFILE ]; then
-  cp $LATESTSNAP $LOGLOC/$BACKFILE
-fi
 
 echo $LATESTSNAP > $LOGLOC/latestsnap.log
 
@@ -18,6 +13,9 @@ fi
 
 diff -q $LOGLOC/latestsnap.log $LOGLOC/priorsnap.log > /dev/null
 if [ $? -ne 0 ] ; then
-  bash $cDIR/cloudsend.sh $LOGLOC/$BACKFILE $BACKDEST
+  cp $LATESTSNAP $LOGLOC/$BACKFILE
+  cd $LOGLOC
+  bash ~/cloudsend.sh $BACKFILE $BACKDEST
+  cd $PWD
   cat $LOGLOC/latestsnap.log > $LOGLOC/priorsnap.log
 fi
