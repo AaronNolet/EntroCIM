1d62d6549fc212218f8b33046924d780438434fa
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2019-03-13 19:11:15 -0400

    cron fixes

--- eCIM Edge/EntroSetup.sh
+++ eCIM Edge/EntroSetup.sh
@@ -7,10 +7,9 @@ trap 'echo "Installer terminated. Exit.";' INT TERM EXIT
 
 #Set Vars
 HOSTNAME=$(hostname)
-set -f
+
 ECRON=$'05 04 * * * $HOME/IoT_Warez/updatescripts.sh; $HOME/scripts/podupdate.sh > /tmp/$HOSTNAME\'_podupdate_\'`date \'+\%b-\%d-\%Y\'`.log 2>&1; $HOME/scripts/sendlog.sh #Added by IoT Warez, LLC'
 RCRON=$'00 04 * * * /home/finstack/scripts/fail2ban-allstatus.sh #Added by IoT Warez, LLC'
-set +f
 
 echo "EntroCIM Installer"
 
@@ -127,7 +126,9 @@ if grep -Fqs "\$HOME/IoT_Warez/updatescripts.sh; \$HOME/scripts/podupdate.sh > /
   echo "Automatic Updates are already enabled..."
 else
   if [ ! -f /var/spool/cron/crontabs/entrocim ]; then
+    set -f
     echo -e "SHELL=/bin/bash\n"$ECRON > /var/spool/cron/crontabs/entrocim
+    set +f
     chown entrocim:crontab /var/spool/cron/crontabs/entrocim
     chmod 600 /var/spool/cron/crontabs/entrocim
   fi
@@ -137,7 +138,9 @@ if grep -Fqs "/home/entrocim/scripts/fail2ban-allstatus.sh #Added by IoT Warez, 
   echo "Automatic Updates are already enabled..."
 else
   if [ ! -f /var/spool/cron/crontabs/root ]; then
+    set -f
     echo -e $RCRON > /var/spool/cron/crontabs/root
+    set +f
     chown root:crontab /var/spool/cron/crontabs/root
     chmod 600 /var/spool/cron/crontabs/root
   fi
