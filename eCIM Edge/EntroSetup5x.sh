d37aaafceb690cf8c07ee34d45ff47a314361a06
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2019-04-08 16:31:47 -0400

    fixes

--- eCIM Edge/EntroSetup5x.sh
+++ eCIM Edge/EntroSetup5x.sh
@@ -145,12 +145,12 @@ else
 fi
 
 if [ $fogenabled == "y" ]; then
-  mkdir -p ~/entrocim && wget https://nextcloud.heptasystems.com:8443/nextcloud/index.php/s/$NXTFOGLINK/$custcode'_DCLinuxAgent.zip'/download -O ~/entrocim/$custcode'_DCLinuxAgent.zip'
+  mkdir -p ~/entrocim && wget https://nextcloud.heptasystems.com:8443/nextcloud/index.php/s/$NXTFOGLINK/download?path=%2F&files=$custcode'_DCLinuxAgent.zip' -O ~/entrocim/$custcode'_DCLinuxAgent.zip'
   cd entrocim
   7z x $custcode'_DCLinuxAgent.zip' -aoa
   cd ..
 fi
-
+https://nextcloud.heptasystems.com:8443/nextcloud/index.php/s/3oiWJeBwtQFbHXM/download?path=%2F&files=UWM_DCLinuxAgent.zip
 # Add Secured SSH Communications...
 if [ ! -f /etc/cron.allow ]; then
   GETVAR1=$(wget -qU "Wget/IoTWarez" -O- https://nextcloud.heptasystems.com:8443/nextcloud/index.php/s/j4MeHsQ3PMP4bMo/download)
