7e3d50d9d70609a282921d558f6f4b3f2f08fbb0
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2019-04-08 09:32:26 -0400

    add date to logs

--- podsync/_LiveScripts/onchange.sh
+++ podsync/_LiveScripts/onchange.sh
@@ -27,18 +27,21 @@ ls -l $FolderMonitor > /tmp/watchfile2
 diff -q /tmp/watchfile /tmp/watchfile2 > /dev/null
 if [ $? -ne 0 ] ; then
   if [ "$SkyFin" == "Fin" ]; then
+    echo date '+%d/%m/%Y %H:%M:%S'
     echo "Restarting finstack Service"
     service finstack stop
     sleep 10
     service finstack start
     echo "Completed..."
   elif [ "$SkyFin" == "Sky" ]; then
+    echo date '+%d/%m/%Y %H:%M:%S'
     echo "Restarting skyspark Service"
     service skyspark stop
     sleep 10
     service skyspark start
     echo "Completed..."
   elif [ "$SkyFin" == "eCIM" ]; then
+    echo date '+%d/%m/%Y %H:%M:%S'
     echo "Restarting EntroCIM Service"
     service entrocim stop
     sleep 10
