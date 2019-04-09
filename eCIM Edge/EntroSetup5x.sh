164e183a9795456305c5057ecc8f06de45762a46
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2019-04-09 19:49:11 -0400

    Modified to use v5.0.1731

--- eCIM Edge/EntroSetup5x.sh
+++ eCIM Edge/EntroSetup5x.sh
@@ -7,7 +7,7 @@ trap 'echo "Installer terminated. Exit.";' INT TERM EXIT
 
 #Set Vars
 HOSTNAME=$(hostname)
-NXTLINK="KeRmPnZSQyosmNM"
+NXTLINK="n6xSKJknC9jBgNd"
 NXTFOGLINK="3oiWJeBwtQFbHXM"
 
 clear
