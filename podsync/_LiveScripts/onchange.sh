bfac9f452f577a4850f6c2a452bdfdd46a7e2f9a
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2019-08-12 18:44:39 -0400

    Adding wait for current service pid to stop prior to starting.

--- podsync/_LiveScripts/onchange.sh
+++ podsync/_LiveScripts/onchange.sh
@@ -7,13 +7,19 @@
 if systemctl cat skyspark.service > /dev/null 2>&1; then
   FolderMonitor=$(systemctl cat skyspark.service |grep WorkingDirectory=/|sed 's/WorkingDirectory=//')/lib/fan/
   SkyFin=Sky
+  PIDname="skyspark.pid"
 elif systemctl cat finstack.service > /dev/null 2>&1; then
   FolderMonitor=$(cat /etc/init.d/finstack |grep HomeFolder=/|sed 's/HomeFolder=//')/lib/fan/
   SkyFin=Fin
+  PIDname="finstack.pid"
 elif systemctl cat entrocim.service > /dev/null 2>&1; then
   FolderMonitor=$(cat /etc/init.d/entrocim |grep HomeFolder=/|sed 's/HomeFolder=//')/lib/fan/
   SkyFin=eCIM
+  PIDname="entrocim.pid"
 fi
+
+PIDFile="/var/run/$PIDname"
+CurPID=$(<"$PIDFile")
 
 # ******************************************************
 # * Monitor Fan Folder for changes and Restart Service *
@@ -30,21 +36,22 @@ if [ $? -ne 0 ] ; then
     echo date '+%d/%m/%Y %H:%M:%S'
     echo "Restarting finstack Service"
     service finstack stop
-    sleep 10
+    wait $CurPID
     service finstack start
     echo "Completed..."
   elif [ "$SkyFin" == "Sky" ]; then
     echo date '+%d/%m/%Y %H:%M:%S'
     echo "Restarting skyspark Service"
     service skyspark stop
-    sleep 10
+    wait $CurPID
     service skyspark start
     echo "Completed..."
   elif [ "$SkyFin" == "eCIM" ]; then
+    echo "$PIDFile ($CurPID)" > /tmp/PIDlog.log
     echo date '+%d/%m/%Y %H:%M:%S'
     echo "Restarting EntroCIM Service"
     service entrocim stop
-    sleep 10
+    wait $CurPID
     service entrocim start
     echo "Completed..."
   fi
