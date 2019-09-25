8a3381d1cd549e3a57507cff3b00015553da9128
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2019-09-25 13:04:28 -0400

    new management scripts added

---
+++ Scripts/toolboxemail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+recipient="s.clark@heptasystems.com,a.nolet@heptasystems.com"
+sender="helpdesk@heptasystems.com"
+ffolder="Toolbox_Topics"
+rndmatt=$( ls $HOME/scripts/Toolbox_Topics/*.pdf | sed 's#.*/##' | shuf -n 1 )
+DATE=`date +%A", "%B" "%d", "%Y`
+
+(echo -e "Attached is todays daily Toolbox meeting.\n\nThank you all for attending today's safety discussions.\n\nIf anyone has questions or concerns on today's meeting, please contact your safety manager.\n\n\nSincerly,\nThe Hepta Control Systems Safety Team"; uuencode "$HOME/scripts/$ffolder/$rndmatt" "$rndmatt") | mail -a "X-Priority:1" -s "HCS Toolbox Meeting - $DATE" $recipient -r $sender
