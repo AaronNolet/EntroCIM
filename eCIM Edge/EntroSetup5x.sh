089a7562b2e7a55aadb94b6b7fbe0519e8901263
AaronNolet <40274249+AaronNolet@users.noreply.github.com>
2019-12-02 19:48:36 -0500

    New 2674 FoS ZIP

--- eCIM Edge/EntroSetup5x.sh
+++ eCIM Edge/EntroSetup5x.sh
@@ -8,11 +8,11 @@ trap 'echo "Installer terminated. Exit.";' INT TERM EXIT
 #Set Vars
 HOSTNAME=$(hostname)
 #EntroCIM NXTLINK Versions:
-# 5.0.3.2674_FoS "QGC6DJLrc8PjrRX"
+# 5.0.3.2674_FoS "rfECRBNHnSqEGik"
 # 5.0.2276 "n8m2PzRSLmiMrap"
 # 5.0.2156 "WBKTP8WxcZAi4Lx"
 # 5.0.2043 "iYetpysixzTPcYQ"
-NXTLINK="QGC6DJLrc8PjrRX"
+NXTLINK="rfECRBNHnSqEGik"
 
 # FOG Code during install needs to match with case start of file followed by _DCLinuxAgent.zip
 NXTFOGLINK="3oiWJeBwtQFbHXM"
